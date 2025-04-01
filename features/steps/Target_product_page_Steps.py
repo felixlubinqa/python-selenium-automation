eebd54319798c92903afcef8935748ee808b81fa
HW_2 Locators <felix.lubin.qa@gmail.com>
2025-04-01 00:05:45 -0400

    HW number 3+5 Revision (#1 for HW can be found in Amazon_CSS_Locator.py)

--- features/steps/Target_product_page_Steps.py
+++ features/steps/Target_product_page_Steps.py
@@ -5,7 +5,9 @@ from selenium.webdriver.support import expected_conditions as EC
 
 
 COLOR_OPTIONS = (By.CSS_SELECTOR, "[aria-label*='Color']")
-SELECTED_COLOR = (By.CSS_SELECTOR, 'div[class*="styles_headerWrapper"]')
+SELECTED_COLOR1 = (By.CSS_SELECTOR, 'div[class*="styles_headerWrapper"]')
+SELECTED_COLOR2 = (By.CSS_SELECTOR, 'div[class*="styles_headerWrapper"]')
+
 
 @Given('Open Target Product A-82711059 Page')
 def open_product_page(context):
@@ -24,26 +26,32 @@ def open_product_page(context):
 
 @Then('Verify color selection is shown on A-82711059 page')
 def click_and_verify_colors(context):
-    color_options = "[aria-label*='Color']"
-    #context.driver.find_element(By.CSS_SELECTOR, color_options).click()
-    color_count = len(context.driver.find_elements(By.CSS_SELECTOR, color_options))
-    print(color_count)
+    actual_colors = []
+    expected_colors = ['Cut & Dry', 'Max Effort', 'Soft Black', 'Struck By Lightning', 'Don\'t Fray Me - Out of Stock', 'Indigo Here We go - Out of Stock']
+    colors = context.driver.find_elements(By.CSS_SELECTOR, "[height='64px']")
 
-    for i in range(color_count):
-        context.driver.find_element(By.CSS_SELECTOR, color_options).click()
-        sleep(2)
+    for color in colors:
+        color.click()
+        sleep(1)
+        selected_color = context.driver.find_element(*SELECTED_COLOR1).text
+
+        selected_color = selected_color.split('\n')[1]
+        actual_colors.append(selected_color)
+        print(actual_colors)
+
+    assert actual_colors == expected_colors, f'Actual Colors:{actual_colors} != Expected colors:{expected_colors}'
+
 
 @Then('Verify color selection is shown on A-93370753 page')
 def click_and_verify_colors(context):
     actual_colors = []
     expected_colors = ['Dark Green', 'Dark Green', 'Navy Blue', 'Red']
-    colors = context.driver.find_elements(*COLOR_OPTIONS) #[green], [navy blue], [red]
-
+    colors = context.driver.find_elements(*COLOR_OPTIONS)
 
     for color in colors:
         color.click()
         sleep(5)
-        selected_color = context.driver.find_element(*SELECTED_COLOR).text
+        selected_color = context.driver.find_element(*SELECTED_COLOR2).text
 
         selected_color = selected_color.split('\n')[1]
         actual_colors.append(selected_color)
--- features/tests/Target_product_details.feature
+++ features/tests/Target_product_details.feature
@@ -3,11 +3,11 @@ Feature: # Enter feature name here
   # Enter feature description here
 
 
-#Scenario: User can select product color 1
-#  Given Open Target Product A-82711059 Page
-#  Then Verify color selection is shown on A-82711059 page
+Scenario: User can select product color 1
+  Given Open Target Product A-82711059 Page
+  Then Verify color selection is shown on A-82711059 page
 
 
-  Scenario: User can select product color 2
-    Given Open Target Product A-93370753 Page
-    Then Verify color selection is shown on A-93370753 page
+Scenario: User can select product color 2
+  Given Open Target Product A-93370753 Page
+  Then Verify color selection is shown on A-93370753 page
